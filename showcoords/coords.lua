6c5692d7a74ad174d6730fb2342361bca82aa10e
Logan Elliott <illusivetea@outlook.com>
2018-10-29 04:05:01 +0000

    Make text smaller

--- showcoords/coords.lua
+++ showcoords/coords.lua
@@ -1,7 +1,7 @@
 function DrawTxt(text, x, y)
 	SetTextFont(0)
 	SetTextProportional(1)
-	SetTextScale(0.0, 0.70)
+	SetTextScale(0.0, 0.4)
 	SetTextDropshadow(1, 0, 0, 0, 255)
 	SetTextEdge(1, 0, 0, 0, 255)
 	SetTextDropShadow()
@@ -20,25 +20,25 @@ Citizen.CreateThread(function()
     	roundy = tonumber(string.format("%.2f", y))
 		roundz = tonumber(string.format("%.2f", z))
 		
-		DrawTxt("~r~X:~s~ "..roundx, 0.33, 0.00)
-		DrawTxt("~r~Y:~s~ "..roundy, 0.44, 0.00)
-		DrawTxt("~r~Z:~s~ "..roundz, 0.55, 0.00)
+		DrawTxt("~r~X:~s~ "..roundx, 0.32, 0.00)
+		DrawTxt("~r~Y:~s~ "..roundy, 0.38, 0.00)
+		DrawTxt("~r~Z:~s~ "..roundz, 0.445, 0.00)
 
 		heading = GetEntityHeading(GetPlayerPed(-1))
 		roundh = tonumber(string.format("%.2f", heading))
-		DrawTxt("~r~Heading:~s~ "..roundh, 0.40, 0.05)
+		DrawTxt("~r~H:~s~ "..roundh, 0.50, 0.00)
 
         local rx,ry,rz = table.unpack(GetEntityRotation(PlayerPedId(), 1))
-		DrawTxt("~r~RX:~s~ "..tonumber(string.format("%.2f", rx)), 0.33, 0.1)
-		DrawTxt("~r~RY:~s~ "..tonumber(string.format("%.2f", ry)), 0.44, 0.1)
-		DrawTxt("~r~RZ:~s~ "..tonumber(string.format("%.2f", rz)), 0.55, 0.1)
+		DrawTxt("~r~RX:~s~ "..tonumber(string.format("%.2f", rx)), 0.38, 0.03)
+		DrawTxt("~r~RY:~s~ "..tonumber(string.format("%.2f", ry)), 0.44, 0.03)
+		DrawTxt("~r~RZ:~s~ "..tonumber(string.format("%.2f", rz)), 0.495, 0.03)
 	
 		speed = GetEntitySpeed(PlayerPedId())
 		rounds = tonumber(string.format("%.2f", speed))
-		DrawTxt("~r~Player Speed: ~s~"..rounds, 0.40, 0.90)
+		DrawTxt("~r~Player Speed: ~s~"..rounds, 0.40, 0.92)
 
 		health = GetEntityHealth(PlayerPedId())
-		DrawTxt("~r~Player Health: ~s~"..health, 0.40, 0.85)
+		DrawTxt("~r~Player Health: ~s~"..health, 0.40, 0.95)
 
 		veheng = GetVehicleEngineHealth(GetVehiclePedIsUsing(PlayerPedId()))
 		vehbody = GetVehicleBodyHealth(GetVehiclePedIsUsing(PlayerPedId()))
@@ -46,11 +46,11 @@ Citizen.CreateThread(function()
 			vehenground = tonumber(string.format("%.2f", veheng))
 			vehbodround = tonumber(string.format("%.2f", vehbody))
 
-			DrawTxt("~r~Engine Health: ~s~"..vehenground, 0.0, 0.73)
+			DrawTxt("~r~Engine Health: ~s~"..vehenground, 0.015, 0.76)
 
-			DrawTxt("~r~Body Health: ~s~"..vehbodround, 0.0, 0.69)
+			DrawTxt("~r~Body Health: ~s~"..vehbodround, 0.015, 0.73)
 
-			DrawTxt("~r~Vehicle Fuel: ~s~"..tonumber(string.format("%.2f", GetVehicleFuelLevel(GetVehiclePedIsUsing(PlayerPedId())))), 0.0, 0.65)
+			DrawTxt("~r~Vehicle Fuel: ~s~"..tonumber(string.format("%.2f", GetVehicleFuelLevel(GetVehiclePedIsUsing(PlayerPedId())))), 0.015, 0.70)
 		end
     end
 end)
